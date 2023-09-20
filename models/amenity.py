100993f6b76e340dab3ddae5025d7224bf282470
RealKingTino <tmowizzy@gmail.com>
2023-09-20 12:25:05 -0700

    updated aminity class

--- models/amenity.py
+++ models/amenity.py
@@ -10,12 +10,3 @@ class Amenity(BaseModel, Base):
     __tablename__ = "amenities"
 
     name = Column(String(128), nullable=False)
-
-    def places(self):
-        """ Returns the list of Place instances with this Amenity """
-        from models import storage
-        place_amenities = []
-        for place in storage.all("Place").values():
-            if self.id in [amenity.id for amenity in place.amenities]:
-                place_amenities.append(place)
-        return place_amenities
