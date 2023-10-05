39b1c6e5cf89f5622cfbbb9b3601a6ee08d23513
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-10-05 16:41:07 -0700

    fixed pycode error

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,18 +4,15 @@ from fabric.api import local
 import os
 from datetime import datetime
 
+
 def do_pack():
-
-
     try:
         now = datetime.now()
         path = "web_static_{}.tgz".format(
             now.strftime("%Y%m%d%H%M%S")
         )
         local("mkdir versions")
-
         output = local("tar -cvzf {} web_static".format(path))
-
         return output
     except Exception as e:
         return None
