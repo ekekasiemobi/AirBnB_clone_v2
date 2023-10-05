6a0c9108d8114251a431b0d2358900c29b553270
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-10-05 07:07:26 -0700

    Bash script that sets up your web servers

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Install Nginx if not already installed
+
+apt-get update
+apt-get install -y nginx
+
+mkdir -p /data/web_static/releases/test
+mkdir -p /data/web_static/shared
+
+sudo echo "Welcome to my World!" | sudo tee /data/web_static/releases/test/index.html
+
+ln -s /data/web_static/releases/test /data/web_static/current
+
+chown -R ubuntu:ubuntu /data/
+
+config_content="
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+
+    server_name _;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+    }
+
+    location / {
+        return 404;
+    }
+}"
+
+sudo service nginx start
