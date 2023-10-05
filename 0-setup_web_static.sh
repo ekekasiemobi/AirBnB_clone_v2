17da80bf6384aca0f5da2c28de95d4443b55c7e2
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-10-05 07:34:27 -0700

    Bash script that sets up your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -22,17 +22,31 @@ chown -R ubuntu:ubuntu /data/
 config_content="
 server{
     listen 80 default_server;
-    listen [::]:80 default_server;
+	listen [::]:80 default_server;
 
-    server_name _;
+	server_name _;
+	index index.html index.htm;
+	error_page 404 /404.html;
+	add_header X-Served-By \$hostname;
 
-    location /hbnb_static {
-        alias /data/web_static/current;
-    }
+	location / {
+		root /var/www/html/;
+		try_files \$uri \$uri/ =404;
+	}
 
-    location / {
-        return 404;
-    }
+	location /hbnb_static/ {
+		alias /data/web_static/current/;
+		try_files \$uri \$uri/ =404;
+	}
+
+	if (\$request_filename ~ redirect_me) {
+		rewrite ^ https://sketchfab.com/bluepeno/models permanent;
+	}
+
+	location = /404.html {
+		root /var/www/error/;
+		internal;
+	}
 }"
 
 sudo service nginx start
