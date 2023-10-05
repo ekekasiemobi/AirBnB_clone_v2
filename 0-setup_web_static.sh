27731139998177b6dd4c6d9cc81f020e73e146e3
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-10-05 07:27:29 -0700

    Bash script that sets up your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,16 +9,18 @@ mkdir -p /data/web_static/shared
 
 sudo echo "Welcome to my World!" | sudo tee /data/web_static/releases/test/index.html
 
-# Create or recreate the symbolic link
 if [ -L /data/web_static/current ]; then
     rm /data/web_static/current
 fi
+
+echo "$config_content" > /etc/nginx/sites-available/my_config
 
 ln -s /data/web_static/releases/test /data/web_static/current
 
 chown -R ubuntu:ubuntu /data/
 
-printf %s "server {
+config_content="
+server{
     listen 80 default_server;
     listen [::]:80 default_server;
 
