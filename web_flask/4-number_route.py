6f90acf436c103d696eb9efef11c234cf38448d0
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-10-22 11:28:48 -0700

    fixes indentation error

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -34,7 +34,7 @@ def python(text='is cool'):
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
     """display “n is a number” only if n is an integer"""
-        return ("{:d} is a number".format(n))
+    return ("{:d} is a number".format(n))
 
 
 if __name__ == '__main__':
